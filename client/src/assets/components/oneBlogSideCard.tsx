bbf9da6296bc5dadc7ad016685779053d25dc44e
sushant <sushantjarial7@gmail.com>
2024-12-06 05:42:36 +0530

    fixed a bug

--- client/src/assets/components/oneBlogSideCard.tsx
+++ client/src/assets/components/oneBlogSideCard.tsx
@@ -26,10 +26,7 @@ const blogId=searchParms.get("id");
           }
 
         );
-        if((res.data.authorBlogs && res.data.authorBlogs.length<2)){
-          hideSide();
-          return;
-        }
+       
         setUserPosts(res.data.authorBlogs)
 
       };
